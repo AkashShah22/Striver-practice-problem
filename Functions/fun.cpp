80089689a027ed2365d5fc4ae6972734907d9bbd
user.name --"Akash Shah <user.email --"akash.shah84099@gmail.com>
2026-09-08 19:53:08 +0530

    array,string,forLoop,whileloop,function

---
+++ Functions/fun.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+    //function are set of code which perform
+    //anything for u
+    //->function are used to modularised code
+    //->function are used to increase readibility
+    //->function are used to use same code multiple times
+    //->void=which doesnt return anything
+    //->return
+    //->parametrised
+    //->non parameterised
+    void printnName(string name){
+        cout<<"hello ";
+    }
+int main(){
+    string name;
+    cin>>name;
+    printnName(name);
+    return 0;
+}
